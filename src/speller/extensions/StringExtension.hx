74c48af87f5040c85e9f5075e09faba5a7ae1cdc
FAL <falworks.contact@gmail.com>
2021-04-10 13:56:50 +0900

    feat: remove extern

--- src/speller/extensions/StringExtension.hx
+++ src/speller/extensions/StringExtension.hx
@@ -4,36 +4,36 @@ class StringExtension {
 	/**
 		Alternative to `toLowerCase()` but returns `LowerCaseString` instead of `String`.
 	**/
-	public static extern inline function toLower(s: String): LowerCaseString
+	public static inline function toLower(s: String): LowerCaseString
 		return LowerCaseString.from(s);
 
 	/**
 		Alternative to `toUpperCase()` but returns `UpperCaseString` instead of `String`.
 	**/
-	public static extern inline function toUpper(s: String): UpperCaseString
+	public static inline function toUpper(s: String): UpperCaseString
 		return UpperCaseString.from(s);
 
 	/**
 		@return `true` if this is an empty string (`""`).
 	**/
-	public static extern inline function isEmpty(s: String): Bool
+	public static inline function isEmpty(s: String): Bool
 		return s.length == 0;
 
 	/**
 		@return `true` if this is not an empty string (`""`).
 	**/
-	public static extern inline function isSome(s: String): Bool
+	public static inline function isSome(s: String): Bool
 		return s.length != 0;
 
 	/**
 		Tries to cast to `NonEmptyString`.
 	**/
-	public static extern inline function toNonEmpty(s: String): Maybe<NonEmptyString>
+	public static inline function toNonEmpty(s: String): Maybe<NonEmptyString>
 		return NonEmptyString.from(s);
 
 	/**
 		Alternative to `trim()` but returns `TrimmedString` instead of `String`.
 	**/
-	public static extern inline function toTrimmed(s: String): TrimmedString
+	public static inline function toTrimmed(s: String): TrimmedString
 		return TrimmedString.from(s);
 }
